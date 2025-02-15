54a7fd0d22f7799d476078f24e0e91b2d27f9022
bunagr <allexanderlinux@gmail.com>
2025-02-15 08:18:18 -0800

    core

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const app = express();
+const port = 3000;
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
